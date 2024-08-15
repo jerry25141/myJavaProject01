c09d9007ac1e259561d11eb1b4a37290c68d8b8d
jerry25141-SP6 <jerry25141@hotmail.com>
2024-08-15 15:28:42 +0800

    📦 NEW: JPA309 迴圈倍數判斷

--- src/java_middle_quiz/lesson03/quiz03_09.java
+++ src/java_middle_quiz/lesson03/quiz03_09.java
@@ -16,34 +16,48 @@ package java_middle_quiz.lesson03;
 //     i = 0
 //     i = 1
 //     i = 3
-//(2) 請用 continue 設計一個程式，能夠計算 1 到「使用者輸入的數」之中，那些數是 3 的倍數或是 5 的倍數，而不是 7 的倍數。
+//(2) 請用 continue 設計一個程式，能夠計算 1 到「使用者輸入的數」之中
+// ，那些數是 3 的倍數或是 5 的倍數，而不是 7 的倍數。
 //(3) 將這些數值相加後輸出，顯示如執行結果參考畫面。
 //--------------------------------------------------------------------------------------------
-
-// import java.util.*;
-
-// public class JPA309a
-// {
-//     public static void main(String argv[])
-//     {
-//         int sum = 0, n = new Scanner(System.in).nextInt();
-//         for (int i = 1; i <= n; i++)
-//         {
-//             if (i % 7 == 0)
-//                 continue;
-//             if (i % 3 == 0 || i % 5 == 0)
-//                 sum += i;
-//         }
-//         System.out.println("Answer: " + sum);
-//     }
-// }
+// Input：
+// 2
+// Answer: 0
+// Input：
+// 5
+// Answer: 8
+// Input：
+// 13
+// Answer: 45
+// Input：
+// 3
+// Answer: 3
 
 //////////////////////////////////////////////////////
 import java.util.*;
+
 //////////////////////////////////////////////////////
 public class quiz03_09 {
     static Scanner myScanner = new Scanner(System.in);
-    public static void main(String[] args) {
-        
+
+    public static void main(String argv[]) {
+        test();
+        test();
+        test();
+        test();
+    }
+    
+    public static void test() {
+        System.out.println("Input：");
+        int sum = 0, n = myScanner.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 7 == 0) // 被7整除，continue跳過for
+                continue;
+            if (i % 3 == 0 || i % 5 == 0)
+                sum += i;
+        }
+
+        System.out.println("Answer: " + sum);
     }
 }
