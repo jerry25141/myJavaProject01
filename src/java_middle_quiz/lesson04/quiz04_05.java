c32d5f274abbcfd6fd3b9009b07a5ebed4f38192
jerry25141-SP6 <jerry25141@hotmail.com>
2024-08-21 10:49:12 +0800

    📦 NEW: JPA405 遞迴函數

--- src/java_middle_quiz/lesson04/quiz04_05.java
+++ src/java_middle_quiz/lesson04/quiz04_05.java
@@ -10,29 +10,34 @@ package java_middle_quiz.lesson04;
 // Input the number n: 50
 // Ans: 2550
 
-// import java.util.*;
-
-// public class JPA405a {
-//     static Scanner keyboard = new Scanner(System.in);
-//     public static void main(String args[]) {
-//         System.out.printf("Input the number n: ");
-//         int n = keyboard.nextInt();
-//         System.out.printf("Ans: %d", sum2(n));
-//     }
-//     static int sum2(int n) {
-//         if (n == 1)
-//             return 2;
-//         else
-//             return sum2(n - 1) + 2 * n;
-//     }
-// }
-
 //////////////////////////////////////////////////////
 import java.util.*;
 //////////////////////////////////////////////////////
 public class quiz04_05 {
     static Scanner myScanner = new Scanner(System.in);
     public static void main(String[] args) {
-        
+        test();
     }
+    public static void test() {
+        System.out.print("Input the number n: ");
+        int n = myScanner.nextInt();
+
+        if (n >= 1) {
+            int total = 1;
+            // 呼叫 遞迴
+            total = run_SUM2(n);
+            System.out.println("Ans: " + total);
+        } else
+            return;
+    }
+/////////////////////////////////////////////////////
+    // !! 遞迴函數 sum2(n) = sum2(n – 1) + 2 * n //
+    public static int run_SUM2(int n) {
+        if (n == 1) {
+            return 2;
+        } else {
+            return run_SUM2(n - 1) + 2 * n;
+        }
+    }
+/////////////////////////////////////////////////////
 }
