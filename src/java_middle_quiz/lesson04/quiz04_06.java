0dc8f4a3a1c629b94a4610f46b3205a4c0079fa6
jerry25141-SP6 <jerry25141@hotmail.com>
2024-08-21 15:11:49 +0800

    📦 NEW: JPA406 遞迴字串計算

--- src/java_middle_quiz/lesson04/quiz04_06.java
+++ src/java_middle_quiz/lesson04/quiz04_06.java
@@ -11,35 +11,35 @@ package java_middle_quiz.lesson04;
 // Input a string: java
 // java has 0 As
 
-// import java.util.*;
-
-// public class JPA406a {
-//     static Scanner keyboard = new Scanner(System.in);
-//     public static void main(String args[]) {
-//         String t;
-//         System.out.printf("Input a string: ");
-//         t = keyboard.nextLine();
-//         System.out.println(t + " has " + countA(t) + " As");
-//         System.out.printf("Input a string: ");
-//         t = keyboard.nextLine();
-//         System.out.println(t + " has " + countA(t) + " As");
-//     }
-//     public static int countA(String str) {
-//         if (str.equals(""))
-//             return 0;
-//         else if (str.substring(0, 1).equals("A"))
-//             return 1 + countA(str.substring(1));
-//         else
-//             return countA(str.substring(1));
-//     }
-// }
-
 //////////////////////////////////////////////////////
 import java.util.*;
 //////////////////////////////////////////////////////
 public class quiz04_06 {
     static Scanner myScanner = new Scanner(System.in);
     public static void main(String[] args) {
-        
+        test();
+        test();
     }
+
+    public static void test() {
+        System.out.println("Input a string: ");
+        String in_str = myScanner.nextLine();
+
+        // 
+        int x = 0;
+        x = countA(in_str);
+
+        System.out.println(in_str + " has " + x + " A(個)s");
+    }
+//////////////////////////////////////////////////////
+    public static int countA(String str) {
+        if (str.equals("")) // 如果 空字串(!! java 判定 str 只能用 .equals)
+            return 0;
+        // 如果 子字串第1個符號 == "A"
+        else if (str.substring(0, 1).equals("A"))
+            return 1 + countA(str.substring(1)); // 回傳 1 + 遞迴子字串(i從1開始)
+        else
+            return countA(str.substring(1)); // 遞迴子字串(i從1開始)
+    }
+//////////////////////////////////////////////////////
 }
--- src/java_middle_quiz/lesson04/test_1.java
+++ src/java_middle_quiz/lesson04/test_1.java
@@ -8,6 +8,15 @@ import java.util.*;
 public class test_1 {
     static Scanner myScanner = new Scanner(System.in);
     public static void main(String[] args) {
-        
+        String str = "abcd1234";
+        System.out.println("數字="+test(str));
+    }
+    public static int test(String str) {
+        if (str.equals("")) // 如果 空字串(!! java 判定 str 只能用 .equals)
+            return 0;
+        else {
+            System.out.println(str);
+            return 1 + test(str.substring(1)); // 遞迴子字串(i從1開始)
+        }
     }
 }
