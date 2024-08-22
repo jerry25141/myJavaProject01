4af9f1210a4f6d10168f42a0ee83f5a5599036c2
jerry25141-SP6 <jerry25141@hotmail.com>
2024-08-22 14:17:00 +0800

    📦 NEW: JPA407 尾端遞迴計算總和

--- src/java_middle_quiz/lesson04/quiz04_07.java
+++ src/java_middle_quiz/lesson04/quiz04_07.java
@@ -12,41 +12,49 @@ package java_middle_quiz.lesson04;
 // 尾端遞迴：18
 // 迴圈：18
 
-// import java.util.*;
-
-// public class JPA407a {
-//     static Scanner keyboard = new Scanner(System.in);
-//     public static void main(String args[]) {
-//         String s;
-//         System.out.print("Input a string of numbers: ");
-//         s = keyboard.nextLine();
-//         System.out.printf("尾端遞迴：%d\n", sumTail(s, 0));
-//         System.out.printf("迴圈：%d\n", sumLoop(s, 0));
-//         System.out.print("Input a string of numbers: ");
-//         s = keyboard.nextLine();
-//         System.out.printf("尾端遞迴：%d\n", sumTail(s, 0));
-//         System.out.printf("迴圈：%d\n", sumLoop(s, 0));
-//     }
-//     public static int sumTail(String str, int n) {
-//         if (str.equals(""))
-//             return n;
-//         else
-//             return sumTail(str.substring(1), n + Integer.parseInt(str.substring(0, 1)));
-//     }
-//     public static int sumLoop(String str, int n) {
-//         for (int i = 0; i < str.length(); i++) {
-//             n += Integer.parseInt(str.substring(i, i + 1));
-//         }
-//         return n;
-//     }
-// }
-
 //////////////////////////////////////////////////////
 import java.util.*;
+
 //////////////////////////////////////////////////////
 public class quiz04_07 {
     static Scanner myScanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        
+        test();
+        test();
     }
+
+    public static void test() {
+        System.out.print("Input a string of numbers: ");
+        String str = myScanner.nextLine();
+
+        System.out.println("尾端遞迴：" + cal_rec(str, 0));
+        System.out.println("迴圈：" + cal_for(str, 0));
+    }
+
+    //////////////////////////////////////////////////////
+    // 尾端遞迴：
+    public static int cal_rec(String str, int n) {
+        // 如果 空字串
+        if (str.equals(""))
+            return n;
+        else {
+            // 提取 str 開頭的 數字(str[0]) -> 數字(Int)
+            int n_next = Integer.parseInt(str.substring(0, 1));
+            // n += n_next，str[1]後面的，切成.substring(1)遞迴傳入cal_rec
+            return cal_rec(str.substring(1), n + n_next);
+        }
+    }
+
+    // 迴圈：
+    public static int cal_for(String str, int n) {
+        // Str 轉換 Char Array
+        for (char c : str.toCharArray()) {
+            // Char 轉換 Int
+            n += Character.getNumericValue(c);
+        }
+        return n;
+    }
+
+    //////////////////////////////////////////////////////
 }
