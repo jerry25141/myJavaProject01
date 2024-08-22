545e1c720dc99d99b5ab0bf380b4085af313cd8f
jerry25141-SP6 <jerry25141@hotmail.com>
2024-08-22 15:33:55 +0800

    📦 NEW: JPA410 遞迴字串替換

--- src/java_middle_quiz/lesson04/quiz04_10.java
+++ src/java_middle_quiz/lesson04/quiz04_10.java
@@ -6,42 +6,49 @@ package java_middle_quiz.lesson04;
 //(2) 程式執行時，顯示【Input a string: 】要求輸入字串，接續顯示【Input a character: 】
 //    要求輸入「被替換」的字元，最後顯示【Input another character: 】要求輸入替換字元。
 //(3) 請利用 replace() 函數進行字串替換，顯示如執行結果參考畫面。
-//----------------------------------------------------------------------------------
+
 // Input a string: windows
 // Input a character: w
 // Input another character: g
 // gindogs
 
-// import java.util.*;
-
-// public class JPA410a {
-//     static Scanner keyboard = new Scanner(System.in);
-//     public static void main(String args[]) {
-//         String s, c1, c2;
-//         System.out.print("Input a string: ");
-//         s = keyboard.nextLine();
-//         System.out.print("Input a character: ");
-//         c1 = keyboard.nextLine();
-//         System.out.print("Input another character: ");
-//         c2 = keyboard.nextLine();
-//         System.out.printf("%s\n", replace(s, c1, c2));
-//     }
-//     public static String replace(String s, String c1, String c2) {
-//         if (s.equals(""))
-//             return "";
-//         else if (s.substring(0, 1).equals(c1))
-//             return c2 + replace(s.substring(1), c1, c2);
-//         else
-//             return s.substring(0, 1) + replace(s.substring(1), c1, c2);
-//     }
-// }
-
 //////////////////////////////////////////////////////
 import java.util.*;
+
 //////////////////////////////////////////////////////
 public class quiz04_10 {
     static Scanner myScanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        
+        test();
     }
+
+    public static void test() {
+        System.out.print("Input a string: ");
+        String str_org = myScanner.nextLine();
+        System.out.print("Input a character: ");
+        char c_old = myScanner.nextLine().charAt(0); // char只會有1個字員
+        System.out.print("Input another character: ");
+        char c_new = myScanner.nextLine().charAt(0); // char只會有1個字員
+
+        // 遞迴字串替換
+        String str_new = rec_replace(str_org, c_old, c_new);
+        System.out.println(str_new);
+    }
+
+    //////////////////////////////////////////////////////
+    // 遞迴字串替換
+    public static String rec_replace(String str, char oldChar, char newChar) {
+        if (str.equals(""))
+            return "";
+        else {
+            // 判斷是否 replace oldChar
+            char firstChar = str.charAt(0);
+            if (firstChar == oldChar)
+                return newChar + rec_replace(str.substring(1), oldChar, newChar);
+            else
+                return firstChar + rec_replace(str.substring(1), oldChar, newChar);
+        }
+    }
+    //////////////////////////////////////////////////////
 }
