4724f8621be0e5cb8df830b33005690adcb8962c
jerry25141-GigaPC <jerry25141@hotmail.com>
2024-08-26 01:59:44 +0800

    📦 NEW: JPA508 泡泡排序法

--- src/java_middle_quiz/lesson05/quiz05_08.java
+++ src/java_middle_quiz/lesson05/quiz05_08.java
@@ -5,7 +5,7 @@ package java_middle_quiz.lesson05;
 //(2) 程式內有一資料陣列 {2, 4, 3, 5, 7, 6, 9, 1}。
 //(3) 請輸出泡泡排序法的比對過程。
 //(4) 顯示如執行結果參考畫面。
-//--------------------------------------------------------------------------------------------------
+
 // 2 3 4 5 6 7 1 9
 // 2 3 4 5 6 1 7 9
 // 2 3 4 5 1 6 7 9
@@ -14,31 +14,40 @@ package java_middle_quiz.lesson05;
 // 2 1 3 4 5 6 7 9
 // 1 2 3 4 5 6 7 9
 
-// public class JPA508a {
-//     public static void main(String[] argv) {
-//         int[] data = {2, 4, 3, 5, 7, 6, 9, 1};  
-//         int temp;        
-//         for(int i = 0;i < data.length - 1;i++) {
-//             for(int j = 0;j < data.length - 1 - i;j++) {
-//                 if(data[j] > data[j + 1]) {
-//                     temp = data[j];
-//                     data[j] = data[j + 1];
-//                     data[j + 1] = temp;
-//                 }
-//             }        	
-//             for(int k : data) 
-//                 System.out.print(" " + k);
-//             System.out.println("");
-//         }
-//     }
-// }
-
 //////////////////////////////////////////////////////
 import java.util.*;
+
 //////////////////////////////////////////////////////
 public class quiz05_08 {
     static Scanner myScanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        
+        test();
     }
+
+    public static void test() {
+        int[] A = { 2, 4, 3, 5, 7, 6, 9, 1 };
+
+        // 氣泡排序法 + 每行輸出
+        int n = A.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - 1; j++) {
+                // SWAP 從小到大排序
+                if (A[j] > A[j + 1]) {
+                    int tmp = A[j];
+                    A[j] = A[j + 1];
+                    A[j + 1] = tmp;
+                }
+            }
+            my_print(A);
+        }
+    }
+
+    //////////////////////////////////////////////////////
+    public static void my_print(int[] A) {
+        for (int each : A)
+            System.out.print(each + " ");
+        System.out.println();
+    }
+    //////////////////////////////////////////////////////
 }
