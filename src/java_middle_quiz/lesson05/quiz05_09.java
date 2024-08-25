ea980ee2b8ec830640a139dba45b1682a308267c
jerry25141-GigaPC <jerry25141@hotmail.com>
2024-08-26 02:10:52 +0800

    📦 NEW: JPA509 選擇排序法

--- src/java_middle_quiz/lesson05/quiz05_09.java
+++ src/java_middle_quiz/lesson05/quiz05_09.java
@@ -12,37 +12,44 @@ package java_middle_quiz.lesson05;
 // 1 2 3 4 5 6 
 // 1 2 3 4 5 6 
 
-// public class JPA509a {
-//     public static void main(String[] argv) {
-//         int[] data = { 1, 3, 2, 5, 4, 6 };
-//         sort(data);
-//     }
-//     public static void sort(int data[]) {
-//         int d, min;
-//         for (int i = 0; i < data.length - 1; i++) {
-//             min = i;
-//             for (int j = i + 1; j < data.length; j++) {
-//                 if (data[min] > data[j]) {
-//                     min = j;
-//                 }
-//             }
-//             d = data[min];
-//             data[min] = data[i];
-//             data[i] = d;
-//             for (int j = 0; j < data.length; j++) {
-//                 System.out.printf("%d ", data[j]);
-//             }
-//             System.out.printf("%n");
-//         }
-//     }
-// }
-
 //////////////////////////////////////////////////////
 import java.util.*;
+
 //////////////////////////////////////////////////////
 public class quiz05_09 {
     static Scanner myScanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        
+        test();
     }
+
+    public static void test() {
+        int[] A = { 1, 3, 2, 5, 4, 6 };
+
+        // Selection sort 開始
+        int swap, min;
+        int n = A.length;
+        for (int i = 0; i < n - 1; i++) {
+            //
+            min = i;
+            // 依照i順序，搜索最小值
+            for (int j = i + 1; j < n; j++)
+                if (A[min] > A[j])
+                    min = j;
+            // SWAP i 從小到大排
+            swap = A[min];
+            A[min] = A[i];
+            A[i] = swap;
+            // 印出每行
+            my_print(A);
+        }
+    }
+
+    //////////////////////////////////////////////////////
+    public static void my_print(int[] A) {
+        for (int each : A)
+            System.out.print(each + " ");
+        System.out.println();
+    }
+    //////////////////////////////////////////////////////
 }
