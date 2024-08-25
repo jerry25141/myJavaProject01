b594fa739f6efbb56dd1c3df572fbd2ede414b94
jerry25141-GigaPC <jerry25141@hotmail.com>
2024-08-26 01:44:01 +0800

    📦 NEW: JPA507 停車費用計算

--- src/java_middle_quiz/lesson05/quiz05_07.java
+++ src/java_middle_quiz/lesson05/quiz05_07.java
@@ -8,55 +8,67 @@ package java_middle_quiz.lesson05;
 //    c. 4 小時以上不足 6 小時，每小時以 80 元計算。
 //    d. 6 小時以上，每小時以 100 元計算。
 //(3) 請輸入停車時數並計算出停車費用，分別計算 2 小時、3 小時、5 小時及 8 小時的應繳費用，顯示如執行結果參考畫面。
-//-------------------------------------------------------------------------------------------------
+
 // 停車時數：2小時
 // 應繳費用：60元整
-// --------------------
+
 // 停車時數：3小時
 // 應繳費用：110元整
-// --------------------
+
 // 停車時數：5小時
 // 應繳費用：240元整
-// --------------------
+
 // 停車時數：8小時
 // 應繳費用：520元整
 
-// public class JPA507a {
-//     public static void main(String[] argv) {
-//         int hours = 0; 
-//         hours = 2;
-//         park(hours);
-//         System.out.println("--------------------");
-//         hours = 3;
-//         park(hours);
-//         System.out.println("--------------------");
-//         hours = 5;
-//         park(hours);
-//         System.out.println("--------------------");
-//         hours = 8;
-//         park(hours);
-//     }
-//     public static void park(int hours) {
-//         int[] hourTable = { 0, 2, 4, 6 }; 
-//         int[] feeTable = { 30, 50, 80, 100 }; 
-//         int fee = 0; 
-//         System.out.println("停車時數：" + hours + "小時");
-//         for (int i = 3; i >= 0; i--) {
-//             if (hours > hourTable[i]) {
-//                 fee += (hours - hourTable[i]) * feeTable[i];
-//                 hours = hourTable[i];
-//             }
-//         }
-//         System.out.println("應繳費用：" + fee + "元整");
-//     }
-// }
-
 //////////////////////////////////////////////////////
 import java.util.*;
+
 //////////////////////////////////////////////////////
 public class quiz05_07 {
     static Scanner myScanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        
+        test();
     }
+
+    public static void test() {
+        int hours = 0;
+
+        // 2小時
+        hours = 2;
+        cal_park(hours);
+        // 3小時
+        hours = 3;
+        cal_park(hours);
+        // 5小時
+        hours = 5;
+        cal_park(hours);
+        // 8小時
+        hours = 8;
+        cal_park(hours);
+    }
+
+    //////////////////////////////////////////////////////
+    public static void cal_park(int hours) {
+        System.out.println("停車時數：" + hours + "小時");
+
+        // 題目停車費 計算範圍 (0 ~ 2)=$30, (2 ~ 4)=$50, (4 ~ 6)=$80, (6 ~ )=$100
+        int[] hour_table = { 0, 2, 4, 6 };
+        int[] fee_table = { 30, 50, 80, 100 };
+        int output_fee = 0;
+
+        // !! 重要 !! i從後面開始的，先算有沒有超過6小時
+        for (int i = 3; i >= 0; i--) {
+            if (hours > hour_table[i]) {
+        // ex: 8 hours, 總和 += (8-6)*fee > (6-4)*fee > (4-2)*fee > (2-0)*fee
+                output_fee += (hours - hour_table[i]) * fee_table[i];
+                hours = hour_table[i]; // 要記得變成 目前最大 hours
+            }
+        }
+        // Output
+        System.out.println("應繳費用：" + output_fee + "元整");
+        System.out.println();
+    }
+    //////////////////////////////////////////////////////
 }
