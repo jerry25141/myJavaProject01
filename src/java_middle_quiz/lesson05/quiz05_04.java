83dc3248b0758192079ea1ff01abf28b547ec821
jerry25141-GigaPC <jerry25141@hotmail.com>
2024-08-26 00:49:00 +0800

    📦 NEW: JPA504 費氏數

--- src/java_middle_quiz/lesson05/quiz05_04.java
+++ src/java_middle_quiz/lesson05/quiz05_04.java
@@ -6,7 +6,7 @@ package java_middle_quiz.lesson05;
 //(2) 請用陣列方式寫出費氏數 0、1、1、2、3、5、8、13、21、34 的程式。
 //(3) 請事先宣告一個大小為 10 的整數陣列，將最前面兩個陣列指定費氏數的初始值，並利用初始值來計算其餘的費氏數。
 //(4) 以分行方式，顯示此費氏數的前 10 個數值。
-//---------------------------------------------------------------------------------------------------
+
 // 1
 // 1
 // 2
@@ -17,26 +17,29 @@ package java_middle_quiz.lesson05;
 // 21
 // 34
 
-// public class JPA504a {
-//     public static void main(String[] argv) {
-//         int fibonacci[] = new int[10];
-//         fibonacci[0] = 0;
-//         fibonacci[1] = 1;
-//         for (int i = 2; i < 10; i++) {
-//             fibonacci[i] = fibonacci[i - 2] + fibonacci[i - 1];
-//         }
-//         for (int i = 0; i < 10; i++) {
-//             System.out.println(fibonacci[i]);
-//         }
-//     }
-// }
-
 //////////////////////////////////////////////////////
 import java.util.*;
+
 //////////////////////////////////////////////////////
 public class quiz05_04 {
     static Scanner myScanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        
+        test();
+    }
+
+    public static void test() {
+        int fibonacci[] = new int[10];
+        // 初始化 [0] [1]
+        fibonacci[0] = 0;
+        fibonacci[1] = 1;
+
+        // 費氏數列
+        for (int i = 2; i < 10; i++)
+            fibonacci[i] = fibonacci[i - 2] + fibonacci[i - 1];
+
+        // Output
+        for (int each : fibonacci)
+            System.out.println(each);
     }
 }
