2bba8fa8b47c194c77df2f9cb479842ddf5a179e
jerry25141-GigaPC <jerry25141@hotmail.com>
2024-08-26 00:36:08 +0800

    📦 NEW: JPA503 矩陣之和

--- src/java_middle_quiz/lesson05/quiz05_03.java
+++ src/java_middle_quiz/lesson05/quiz05_03.java
@@ -6,7 +6,7 @@ package java_middle_quiz.lesson05;
 //    int B[][] = {{7, 8, 9}, {10, 11, 12}}
 //(2) 計算矩陣 A 與 B 的和，並把相加後的結果放在矩陣 C 裡。
 //(3) 顯示如執行結果參考畫面。
-//--------------------------------------------------------------------------------------
+
 // 陣列A的內容為(3x3)：
 // 01 02 03 
 // 04 05 06 
@@ -19,44 +19,70 @@ package java_middle_quiz.lesson05;
 // 08 10 12 
 // 14 16 18 
 
-// public class JPA503a {
-//     final static int ROW = 2;
-//     final static int COL = 3;
-//     public static void main(String args[]) {
-//         int A[][] = { { 1, 2, 3 }, { 4, 5, 6 } };
-//         int B[][] = { { 7, 8, 9 }, { 10, 11, 12 } };
-//         int C[][] = new int[ROW][COL];
-//         System.out.printf("陣列A的內容為(3x3)：\n");
-//         show(A);
-//         System.out.printf("\n陣列B的內容為(3x3)：\n");
-//         show(B);
-//         add(A, B, C);
-//         System.out.printf("\n陣列A+B=C，陣列C的內容為(3x3)：\n");
-//         show(C);
-//     }
-//     public static void add(int Array1[][], int Array2[][], int Array3[][]) {
-//         for (int i = 0; i < ROW; i++) {
-//             for (int j = 0; j < COL; j++) {
-//                 Array3[i][j] = Array1[i][j] + Array2[i][j];
-//             }
-//         }
-//     }
-//     public static void show(int Array[][]) {
-//         for (int i = 0; i < ROW; i++) {
-//             for (int j = 0; j < COL; j++) {
-//                 System.out.printf("%02d ", Array[i][j]);
-//             }
-//             System.out.printf("%n");
-//         }
-//     }
-// }
-
 //////////////////////////////////////////////////////
 import java.util.*;
+
 //////////////////////////////////////////////////////
 public class quiz05_03 {
     static Scanner myScanner = new Scanner(System.in);
+
+    //////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////
     public static void main(String[] args) {
-        
+        test();
     }
+
+    //////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////
+    // !! 重點 !! 先行宣告 行列大小 public static //
+    public static int Col_len = 2, Row_len = 3; // 兩行，三列
+
+    public static void test() {
+        int A[][] = { { 1, 2, 3 }, { 4, 5, 6 } };
+        int B[][] = { { 7, 8, 9 }, { 10, 11, 12 } };
+
+        // A矩陣
+        System.out.println("陣列A的內容為(3x3)：");
+        my_matrix_print(A);
+        // B矩陣
+        System.out.println("\n陣列B的內容為(3x3)：");
+        my_matrix_print(B);
+        // C矩陣
+        System.out.println("\n陣列A+B=C，陣列C的內容為(3x3)：");
+        my_matrix_print(matrix_cal(A, B));
+    }
+
+    //////////////////////////////////////////////////////
+    // 矩陣相加
+    public static int[][] matrix_cal(int[][] A, int[][] B) {
+        int[][] C = new int[2][3];
+
+        for (int i = 0; i < Col_len; i++) {
+            for (int j = 0; j < Row_len; j++) {
+                C[i][j] = A[i][j] + B[i][j];
+            }
+        }
+        return C;
+    }
+
+    //////////////////////////////////////////////////////
+    // 各位數字，前面加上0
+    public static String my_num_format(int input) {
+        if (input < 10)
+            return "0" + input;
+        else
+            return "" + input;
+    }
+
+    //////////////////////////////////////////////////////
+    // 印出 matrix 矩陣
+    public static void my_matrix_print(int[][] matrix) {
+        for (int i = 0; i < Col_len; i++) {
+            for (int j = 0; j < Row_len; j++) {
+                System.out.print(my_num_format(matrix[i][j]) + " ");
+            }
+            System.out.println();
+        }
+    }
+    //////////////////////////////////////////////////////
 }
